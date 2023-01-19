32eab9ec140f3a5c306bc2001acba7f94c8783fa
Kyakivanfrank <kyakivanfrank@gmail.com>
2023-01-19 22:21:49 +0300

    debugging 2

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+// import React, {useState} from "react";
+import React from "react";
 import illustration from "../../resources/frank_Illustration.svg";
 
 const LandingPage =()=> {
