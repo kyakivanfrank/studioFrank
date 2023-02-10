bc62e14e2b9f8c0a1cef4af7e799e3a793781a19
Kyakivanfrank <kyakivanfrank@gmail.com>
2023-02-11 00:15:24 +0300

    more on price lists

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,35 +1,15 @@
-// import React, {useState} from "react";
 import React from "react";
 import illustration from "../../resources/frank_Illustration.svg";
 
 const LandingPage =()=> {
 
-    // const [display, setDisplay] = useState("none")
-    // const [read_more, setRead_more] = useState("read more ...")
-
-
-// const toggler =() => {
-//     setDisplay( display === "none" ? "block" : "none" )
-//     setRead_more(read_more === "read more ..." ? " ..." : "read more ..." )}
-    
     return <div className="landingPage">
         <img src={illustration} alt="landing-page-illustration" />
         <div className="container">
     <div className="helloText">
     <h1>SEEMLESS USER INTERFACES FOR WEB AND MOBILE APPS</h1>
     <p>Frontend Developer with years of experience in javascript</p>
-
-    <>
-     {/* <p>
-    The internet is cool for you, isn't it? The social media rush! and all those cool new apps you play around with to push your entreprenual products / services to  your visitors. 
-    <span style={{display: display}} id="read_more">
-    Well, how about stepping your game a bar up and own a custom Landing page for what you do. <em>website</em>, <em>Mobile app</em> or <em>online shop</em>.
-    </span>
-    <span onClick={()=> toggler() } style={{color: "yellow",  fontSize:"14px"}}>  {read_more} </span><br/>
-    </p>
-    <p>As you've reached out, we'll create solutions for you quite affordably</p> */}
-    </>
-    <span className="button"><a href="/plans" className="btn btn-white">Let's build your project </a></span>
+     <a href="/plans" className="btn btn-white">Let's build your project</a>
     </div>
     </div>
     </div>
--- src/components/pages/Plans.js
+++ src/components/pages/Plans.js
@@ -3,12 +3,13 @@ import mobile from '../../resources/images/workType/mobile.svg'
 import pc from '../../resources/images/workType/pc.svg'
 import graphics from '../../resources/images/workType/graphics.svg'
 import arrow  from '../../resources/arrow.svg'
+import price_tag from '../../resources/price-tag.gif'
 
 
 const Plans = ()=> <div className='container'>
 <div className="plans">
 
-<h1>Let's build it for you</h1>
+<h1>My line of work</h1>
 
 <div className="plans_box">
 <span>
@@ -19,7 +20,7 @@ const Plans = ()=> <div className='container'>
     <li> <h3>great landing-pages</h3> </li>
     <li> <h3>Responsive designs</h3> </li>
     <li> <h3>search engine optimization</h3> </li>
-    <li> <h3>+<span>$100</span></h3> </li>
+    <li className='price'><span className='pricespan'>starting from <h1>370<span>000Ugx</span></h1></span><img src={price_tag}  alt='price_img'/> </li>
 </ul>
 </div>
 </span>
@@ -32,7 +33,7 @@ const Plans = ()=> <div className='container'>
     <li> <h3>"Easy to navigate" UI</h3> </li>
     <li> <h3>Fast interactivity</h3> </li>
     <li> <h3>Simple but scalable</h3> </li>
-    <li> <h3>$300</h3> </li>
+    <li><img src={price_tag}  alt='price_img'/></li>
 </ul>
 </div>
 </span>
@@ -45,7 +46,7 @@ const Plans = ()=> <div className='container'>
     <li> <h3>price</h3> </li>
     <li> <h3>storage</h3> </li>
     <li> <h3>performance</h3> </li>
-    <li> <h3>mainteinance</h3> </li>
+    <li><img style={{width:"-webkit-fill-available", padding:"0 20px"}} src={price_tag}  alt='price_img'/> </li>
 </ul>
 </div>
 </span>
