64890d50872484b4613dd3fcc1a7356331d53420
Kyakivanfrank <kyakivanfrank@gmail.com>
2023-08-08 12:43:29 +0300

    more

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
 import Contact  from "./components/pages/Contact";
 import Plans from "./components/pages/Plans";
+import whatsapp from "./resources/whatsapp.svg"
 
 import { Switch, Route } from "react-router-dom"
 import ModalOverlay  from "./components/pages/ModalOverlay";
@@ -24,6 +25,10 @@ render(){
         <Route exact path={`/portfolio/:active_tab/:index/:eachItem`} render={ ({match}) => <ModalOverlay {...match}  /> } /> 
 
         <div className="main">
+        <a href="https://wa.me/256708481316?text=_This%20is%20an%20automatic%20message%20from%20frankkyakusse.com_">
+            <img className="floatingWwhatsappBTN" src={whatsapp} alt="whatsapp" />
+            </a>
+
         <Menu/>
         <Header/>
         <Switch>
